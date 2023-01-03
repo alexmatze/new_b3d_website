b2fa6dd71eb0506fb25a36289ba872ef440b23d7
Alexander Kappes <akappes@mpifr-bonn.mpg.de>
2023-01-03 16:30:09 +0100

    upload current status

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+var toggler = document.getElementById("toggle-container");\nvar toggleContents = document.getElementById("checkbox_toggle");\ndocument.addEventListener("click", function(){\n//Apply a class intance to each navigation and set display to toggle: \n toggleContents.classList.toggle("displayed");\n});\n
+
+  // Get the button:
+  let mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
